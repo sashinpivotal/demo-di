aaad08f6e7b99e623478d6a7a6785f74dd4fe446
Sang Shin <sashin@vmware.com>
2021-05-14 07:31:14 -0400

    use property file for message value

--- src/main/java/com/example/demodi/MyMessageProvider.java
+++ src/main/java/com/example/demodi/MyMessageProvider.java
@@ -2,16 +2,22 @@ package com.example.demodi;
 
 import com.example.customAnnotations.MySpecialService;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 @Component("mine")
 @Profile("myProfile")
+@PropertySource("classpath:/my.properties")
 public class MyMessageProvider implements MessageProvider {
+
+    @Value("${message}")
+    private String message;
 
     @Override
     public String getMessage() {
-        return "My hello!";
+        return message;
     }
 
 }
